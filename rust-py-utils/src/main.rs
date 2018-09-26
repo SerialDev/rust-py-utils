2dfe7a949885e61283e1a4363af6ee62201afa6f
Serialdev <carlos.mariscal.melgar@gmail.com>
2018-09-26 15:40:54 +0300

    [+] Uniquify a list

---
+++ rust-py-utils/src/main.rs
@@ -0,0 +1,19 @@
+
+use std::collections::HashMap;
+
+pub fn  uniquify_list(data:&Vec<i64>)-> Vec<i64>{
+    let mut seen: HashMap<i64, i64> = HashMap::new();
+    let mut result:Vec<i64> = Vec::new();
+    for item in data.iter(){
+        if !seen.contains_key(item){
+            seen.insert(*item, 0);
+            result.push(*item);
+        }
+        println!("{}", item);}
+    return result;
+}
+
+
+fn main() {
+    println!("Hello, world!");
+}
